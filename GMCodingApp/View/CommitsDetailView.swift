d1a4077e70e697132bfbd1287264f9da5c498468
Karthik Kannan <ecekarthik15@gmail.com>
2021-02-03 22:53:23 -0500

    (UI): added commits details View Controller

---
+++ GMCodingApp/View/CommitsDetailView.swift
@@ -0,0 +1,97 @@
+//
+//  commitsDetailView.swift
+//  GMCodingApp
+//
+//  Created by Karthik Kannan on 2/3/21.
+//
+
+import UIKit
+import SnapKit
+
+class CommitsDetailView: UIView {
+    lazy var cView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    lazy var vStackView: UIStackView = {
+        let stackview = UIStackView()
+        stackview.axis = .vertical
+        stackview.spacing = 2
+        stackview.alignment = .leading
+        stackview.distribution = .fill
+        return stackview
+    }()
+    
+    lazy var shaLabel: UILabel = {
+        let label = UILabel()
+        label.isUserInteractionEnabled = true
+        label.accessibilityIdentifier = "commits.tableview.cell.sha"
+        label.textAlignment = .left
+        label.numberOfLines = 2
+        label.font = label.font.withSize(10)
+        return label
+    }()
+    
+    lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.isUserInteractionEnabled = true
+        label.accessibilityIdentifier = "commits.tableview.cell.message"
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.font = label.font.withSize(12)
+        return label
+    }()
+    
+    lazy var authorLabel: UILabel = {
+        let label = UILabel()
+        label.isUserInteractionEnabled = true
+        label.accessibilityIdentifier = "commits.tableview.cell.author"
+        label.textAlignment = .left
+        label.numberOfLines = 2
+        label.font = label.font.withSize(12)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        buildView()
+        configureView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func buildView() {
+        
+        backgroundColor = .clear
+        addSubview(cView)
+        cView.addSubview(vStackView)
+        
+        [shaLabel, messageLabel, authorLabel].forEach {
+            vStackView.addArrangedSubview($0)
+        }
+    }
+    
+    func configureView() {
+        cView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(5)
+            make.bottom.equalToSuperview().inset(5)
+            make.leading.equalToSuperview().offset(5)
+            make.trailing.equalToSuperview().inset(5)
+        }
+        
+        vStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.left.right.equalToSuperview()
+        }
+    }
+    
+    func configureData(commit: CommitsModel) {
+        shaLabel.text = commit.hash
+        messageLabel.text = commit.message
+        authorLabel.text = commit.author
+    }
+}
---
+++ GMCodingApp/ViewControllers/CommitsDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CommitsDetailsViewController.swift
+//  GMCodingApp
+//
+//  Created by Karthik Kannan on 2/3/21.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+
+class CommitsDetailsViewController: BaseViewController<CommitsDetailView> {
+    
+}
