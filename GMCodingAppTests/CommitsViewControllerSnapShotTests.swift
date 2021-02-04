d19d438f814ab87b92ad4e1dcc364a7d9f8d7b18
Karthik Kannan <ecekarthik15@gmail.com>
2021-02-03 21:07:24 -0500

    (snapshot): snapshot test added for commits view controller

--- GMCodingAppTests/CommitsViewControllerSnapShotTests.swift
+++ GMCodingAppTests/CommitsViewControllerSnapShotTests.swift
@@ -11,4 +11,27 @@ import SnapshotTesting
 @testable import GMCodingApp
 
 class CommitsViewControllerSnapShotTests: XCTestCase {
+    var subject: CommitsViewController!
+    var fakeGitCommitsViewModel: FakeGitCommitsViewModel!
+    override func setUp() {
+        fakeGitCommitsViewModel = FakeGitCommitsViewModel()
+        subject = CommitsViewController(gitCommitsViewModel: fakeGitCommitsViewModel)
+    }
+
+    func testDealsViewController() {
+        let commit = CommitsModel(author: "Test", hash: "123", message: "test")
+        let commit2 = CommitsModel(author: "Test2", hash: "1234", message: "test2")
+        
+        let nc = UINavigationController(rootViewController: subject)
+        nc.pushViewController(subject, animated: true)
+        subject.view.frame = UIScreen.main.bounds
+        subject.view.layoutIfNeeded()
+        fakeGitCommitsViewModel.getCommitsSubject.onNext([commit, commit2])
+        assertSnapshot(matching: subject, as: .image)
+    }
+    
+    override func tearDown() {
+        fakeGitCommitsViewModel = nil
+        subject = nil
+    }
 }
